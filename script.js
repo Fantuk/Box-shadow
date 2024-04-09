43df3e0d7ff406b36be6b9cfa922f917dc2563f8
Kirillchik <kirillsvirid2006@gmail.com>
2024-04-09 12:57:22 +0700

    did shadow parameters

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const sidebar = document.getElementById("sidebar");
 
 const showCodeBtn = document.getElementById("showCodeBtn");
 const arrow = document.getElementById("arrow");
-const addShadowBtn = document.getElementById("addShadowBtn");
+const shadowArrow = document.getElementById("shadowArrow");
 const palette = document.getElementById("palette");
 
 const blockContainer = document.getElementById("blockContainer");
 const block = document.getElementById("block");
 const boxMenu = document.getElementById("boxMenu");
+const shadowMenu = document.getElementById("shadowMenu");
 
 const inputWidth = document.getElementById("boxWidth");
 const inputHeight = document.getElementById("boxHeight");
@@ -20,6 +21,17 @@ const rangeRadius = document.getElementById("borderRadiusRange");
 
 const borderColor = document.getElementById("borderPallete");
 const boxColor = document.getElementById("boxPallete");
+const shadowColor = document.getElementById("shadowPallete");
+
+const shadowX = document.getElementById("shadowX");
+const shadowY = document.getElementById("shadowY");
+const shadowSpread = document.getElementById("shadowSpread");
+const shadowBlur = document.getElementById("shadowBlur");
+
+const shadowXrange = document.getElementById("shadowXrange");
+const shadowYrange = document.getElementById("shadowYrange");
+const shadowBlurRange = document.getElementById("shadowBlurRange");
+const shadowSpreadRange = document.getElementById("shadowSpreadRange");
 
 // Color change
 
@@ -82,6 +94,30 @@ const boxColor = document.getElementById("boxPallete");
             boxColor.style.transform = "scale(1, 1)";
         });
     // Box BG end
+
+    // Box BG
+        shadowColor.addEventListener("input", (event) => 
+        {
+            block.style.boxShadow = `${shadowX.value || shadowXrange.value}px
+            ${shadowY.value || shadowYrange.value}px
+            ${shadowBlur.value || shadowBlurRange.value}px
+            ${shadowSpread.value || shadowSpreadRange.value}px
+            ${event.target.value}`;
+        });
+
+        shadowColor.addEventListener("mouseenter", () =>
+        {
+            shadowColor.style.transition = "0.5s";
+            shadowColor.style.transform = "scale(1.2, 1.2)"
+            shadowColor.style.opacity = "0.95";
+        });
+
+        shadowColor.addEventListener("mouseleave", () =>
+        {
+            shadowColor.style.transition = "0.5s";
+            shadowColor.style.transform = "scale(1, 1)";
+        });
+    // Box BG end
 // End color change
 
 
@@ -113,34 +149,6 @@ const boxColor = document.getElementById("boxPallete");
             showCodeBtn.style.backgroundColor = "var(--clr2)";
         });
     // Show code button end
-
-    // Add shadow button
-    addShadowBtn.addEventListener("mouseenter", () =>
-    {
-        addShadowBtn.style.transition = "0.5s";
-        addShadowBtn.style.transform = "scale(1.1, 1.1)"
-        addShadowBtn.style.opacity = "0.95";
-    });
-
-    addShadowBtn.addEventListener("mouseleave", () =>
-    {
-        addShadowBtn.style.transition = "0.5s";
-        addShadowBtn.style.transform = "scale(1, 1)";
-        addShadowBtn.style.opacity = "1";
-    });
-
-    addShadowBtn.addEventListener("mousedown", () => 
-    {
-        addShadowBtn.style.transition = "0.1s";
-        addShadowBtn.style.backgroundColor = "var(--clr5)";
-    });
-
-    addShadowBtn.addEventListener("mouseup", () => 
-    {
-        addShadowBtn.style.backgroundColor = "var(--clr2)";
-    });
-    // Add shadow button end
-
 // Buttons animation end
 
 
@@ -228,64 +236,108 @@ const boxColor = document.getElementById("boxPallete");
 // Box custom end
 
 // Shadow custom 
-        // Create custom block
-            const createShadowCustomBlock = (num) =>
+    // Shadow custom menu
+        const createShadowMenu = (e) => 
+        {
+            x = e.clientX + 35;
+            y = e.clientY;
+            shadowMenu.style.left = x + "px";
+            shadowMenu.style.top = "187px";
+        }
+
+        shadowArrow.addEventListener("click", (ev) =>
+        {
+            shadowArrow.classList.toggle("arrowAnimate");
+            shadowMenu.classList.toggle("active");
+            createShadowMenu(ev);
+        });   
+    // Shadow custom menu end
+
+    // Shadow parameters
+        // Shadow X
+            function updateX()
             {
-                const customBlock = document.createElement("div");
-                
-                customBlock.innerHTML = 
-                `<div class = "customContainer">
-                    <p class="customText">Shadow ${num}</p>
-                    <img class="shadowArrow" src="./img/Arrow.svg" alt="netu">
-                </div>`;
-
-                if (num === 2)
-                {
-                    sidebar.style.overflowY = "scroll";
-                }
-
-                sidebar.appendChild(customBlock);
+                shadowXrange.value = shadowX.value;
+                const xVal = shadowX.value || shadowXrange.value;
+                block.style.boxShadow = `${xVal}px ${shadowY.value}px ${shadowBlur.value}px ${shadowSpread.value}px 
+                ${shadowColor.value}`;
             }
 
-            const shadowMenu = document.createElement("div");
-            const createShadowMenu = (e) =>
+            function updateXrange()
             {
-                shadowMenu.classList.add("boxMenu");
-                shadowMenu.innerHTML = 
-                `
-                <p class="customText">Parameters</p>
-                `;
-
-                root.appendChild(shadowMenu);
-
-                x = e.clientX + 35;
-                y = e.clientY - 28;
-                shadowMenu.style.left = `${x}px`;
-                shadowMenu.style.top = y + "px"; 
+                shadowX.value = shadowXrange.value;
+                const xVal = shadowX.value || shadowXrange.value;
+                block.style.boxShadow = `${xVal}px ${shadowYrange.value}px ${shadowBlurRange.value}px 
+                ${shadowSpreadRange.value}px ${shadowColor.value}`;
             }
 
-            let clicks = 0;
-            addShadowBtn.addEventListener("click", (ev) => 
+            shadowX.addEventListener("input", updateX);
+            shadowXrange.addEventListener("input", updateXrange);
+        // Shadow X end
+        
+        // Shadow Y
+            function updateY()
             {
-                clicks++;
-                createShadowCustomBlock(clicks);
-                let arrows = document.getElementsByClassName("shadowArrow");
+                shadowYrange.value = shadowY.value;
+                const yVal = shadowY.value || shadowYrange.value;
+                block.style.boxShadow = `${shadowX.value}px ${yVal}px ${shadowBlur.value}px ${shadowSpread.value}px 
+                ${shadowColor.value}`;
+            }
 
-                // Shadow custom
-                    Array.from(arrows).map((ar) =>
-                    {
-                        ev.stopPropagation();
-                        ar.addEventListener("click",(ev) =>
-                        {
-                            ar.classList.toggle("arrowAnimate");
-                            shadowMenu.classList.toggle("active");
-                            createShadowMenu(ev);
-                        })
-                    });
-                // Shadow custom end
+            function updateYrange()
+            {
+                shadowY.value = shadowYrange.value;
+                const yVal = shadowY.value || shadowYrange.value;
+                block.style.boxShadow = `${shadowX.value}px ${yVal}px ${shadowBlurRange.value}px 
+                ${shadowSpreadRange.value}px ${shadowColor.value}`;
+            }
 
-            });
+            shadowY.addEventListener("input", updateY);
+            shadowYrange.addEventListener("input", updateYrange);
+        // Shadow Y end
 
-        // Create custom block end
-            
+        // Shadow Blur
+            function updateBlur()
+            {
+                shadowBlurRange.value = shadowBlur.value;
+                const blurVal = shadowBlur.value || shadowBlurRange.value;
+                block.style.boxShadow = `${shadowX.value}px ${shadowY.value}px ${blurVal}px ${shadowSpread.value}px 
+                ${shadowColor.value}`;
+            }
+
+            function updateBlurRange()
+            {
+                shadowBlur.value = shadowBlurRange.value;
+                const blurVal = shadowBlur.value || shadowBlurRange.value;
+                block.style.boxShadow = `${shadowX.value}px ${shadowYrange.value}px ${blurVal}px 
+                ${shadowSpreadRange.value}px ${shadowColor.value}`;
+            }
+
+            shadowBlur.addEventListener("input", updateBlur);
+            shadowBlurRange.addEventListener("input", updateBlurRange);
+        // Shadow Blur end
+
+        // Shadow Spread
+            function updateSpread()
+            {
+                shadowSpreadRange.value = shadowSpread.value;
+                const spreadVal = shadowSpread.value || shadowSpreadRange.value;
+                block.style.boxShadow = `${shadowX.value}px ${shadowY.value}px ${shadowBlur.value}px ${spreadVal}px 
+                ${shadowColor.value}`;
+            }
+
+            function updateSpreadRange()
+            {
+                shadowSpread.value = shadowSpreadRange.value;
+                const spreadVal = shadowSpread.value || shadowSpreadrange.value;
+                block.style.boxShadow = `${shadowX.value}px ${shadowYrange.value}px ${shadowBlurRange.value}px 
+                ${spreadVal}px ${shadowColor.value}`;
+            }
+
+            shadowSpread.addEventListener("input", updateSpread);
+            shadowSpreadRange.addEventListener("input", updateSpreadRange);
+        // Shadow Blur end
+
+
+    // Shadow parameters end
 // Shadow custom end
